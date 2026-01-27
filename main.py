7bfd129143d020bc6d4e5597bec2a7b293ef3d64
victor256sd <victor256@gmail.com>
2026-01-26 18:25:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1695,19 +1695,19 @@ if st.session_state.get('authentication_status'):
         Q_total = Q_connection + Q_inclusion + Q_happy_feelings - Q_isolation - Q_bad_feelings
 
         if Q_total >= 20:
-            st.write(f"#### Total Score: {Q_total}")
+            st.write(f"#### Total Score: ## {Q_total}")
             Q_response = "Thriving - Strong emotional reserves and high resilience."
         elif Q_total >= 0 and Q_total <= 19:
-            st.write(f"#### Total Score: {Q_total}")
+            st.write(f"#### Total Score: ## {Q_total}")
             Q_response = "Stable - Typical emotional ups and downs."
         elif Q_total >= -10 and Q_total <= -1:
-            st.write(f"#### Total Score: {Q_total}")
+            st.write(f"#### Total Score: ## {Q_total}")
             Q_response = "At Risk - The child is experiencing more distress than joy."
         elif Q_total <= -11:
-            st.write(f"#### Total Score: {Q_total}")
+            st.write(f"#### Total Score: ## {Q_total}")
             Q_response = "High Distress - May require immediate clinical or school intervention."
 
-        st.write(f"#### Score Interpretation: {Q_response}")
+        st.write(f"#### Score Interpretation: ## {Q_response}")
 
         if (Q36 == "Often" or Q36 == "A lot of the time (almost always)") and Q_connection >= 14:
             st.write(f"The Victimization Gap - Question 36 is high ({Q36}) and the Connection Score ({Q_connection}) is also high. *Recommendation*: Investigate \"toxic\" friendships or bullying within a close group.")
