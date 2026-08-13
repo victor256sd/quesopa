6b59f5b0b8b03516b3b99256d01729ddc6e1d6f1
victor256sd <victor256@gmail.com>
2026-08-13 07:45:18 -0700

    Update include statements for file search results

--- main.py
+++ main.py
@@ -5494,7 +5494,7 @@ if st.session_state.get('authentication_status'):
                             "type": "file_search",
                             "vector_store_ids": [VECTOR_STORE_ID],
                 }],
-                include=["output[*].file_search_call.search_results"]
+                include=["file_search_call.results"]
             )
         # Write response to the answer column.    
         # with answer_col:
@@ -5574,7 +5574,7 @@ if st.session_state.get('authentication_status'):
                             "type": "file_search",
                             "vector_store_ids": [VECTOR_STORE_ID],
                 }],
-                include=["output[*].file_search_call.search_results"]
+                include=["file_search_call.results"]
             )
         # Write response to the answer column.    
         # with answer_col:
@@ -5654,7 +5654,7 @@ if st.session_state.get('authentication_status'):
                             "type": "file_search",
                             "vector_store_ids": [VECTOR_STORE_ID],
                 }],
-                include=["output[*].file_search_call.search_results"]
+                include=["file_search_call.results"]
             )
         # Write response to the answer column.    
         # with answer_col:
@@ -5731,7 +5731,7 @@ if st.session_state.get('authentication_status'):
                             "type": "file_search",
                             "vector_store_ids": [VECTOR_STORE_ID],
                 }],
-                include=["output[*].file_search_call.search_results"]
+                include=["file_search_call.results"]
             )
         # Write response to the answer column.    
         # with answer_col:
@@ -5830,7 +5830,7 @@ if st.session_state.get('authentication_status'):
                             "type": "file_search",
                             "vector_store_ids": [VECTOR_STORE_ID],
                 }],
-                include=["output[*].file_search_call.search_results"]
+                include=["file_search_call.results"]
             )
         # Write response to the answer column.    
         # with answer_col:
@@ -6072,7 +6072,7 @@ if st.session_state.get('authentication_status'):
                             "type": "file_search",
                             "vector_store_ids": [VECTOR_STORE_ID],
                 }],
-                include=["output[*].file_search_call.search_results"]
+                include=["file_search_call.results"]
             )
         # Write response to the answer column.    
         # with answer_col:
